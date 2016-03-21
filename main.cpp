df544eb4bc0610881c775a520bad6bae9a4ebc67
Dmitry Ermachenkov <korvin14@gmail.com>
2016-03-21 20:08:02 +0300

    start of 1st programme

---
+++ main.cpp
@@ -0,0 +1,20 @@
+/*
+ * algo_tp_1/mai.cpp
+ *
+ *   Created on: Mar 21, 2016
+ *       Author: Dmitry Ermachenkov
+ *      License: free-to-use if you refernce me
+ *  Description:
+ */
+
+
+#include <iostream>
+
+using std::cout;
+using std::cin;
+using std::endl;
+
+int main(void) {
+  cout << "Hello bitches" << endl;
+  return 0;
+}
